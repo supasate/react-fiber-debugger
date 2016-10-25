1c11eac183efc87279ea3796573b34c62f134c5e
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-25 21:35:51 +0700

    Remove redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import ReactNoop from 'react-dom/fiber-noop';
 import prettyFormat from 'pretty-format';
 import reactElementPlugin from 'pretty-format/plugins/ReactElement';
+import describeFibers from './describeFibers';
 
-function App({ fibers }) {
-  return (
-    <pre>
-      {prettyFormat(
-        fibers,
-        {
-          plugins: [reactElementPlugin]
-        }
-      )}
-    </pre>
-  );
-}
-
-function mapStateToProps(state) {
+function getState(root) {
+  if (!root) {
+    return {
+      isMounted: false,
+    };
+  }
   return {
-    fibers: state,
-  };
+    isMounted: true,
+    ...describeFibers(root.current)
+  }
 }
 
-export default connect(mapStateToProps)(App);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.renderer = ReactNoop.create({
+      onMountContainer: (root) => {
+        this.root = root;
+      },
+
+      onBeginWork: () => {
+        this.setState(getState(this.root))
+      },
+
+      onCompleteWork: () => {
+        this.setState(getState(this.root))
+      },
+    });
+    this.state = getState(this.root);
+  }
+
+  componentDidMount() {
+    this.props.render(this.renderer);
+  }
+
+  render() {
+    return (
+      <pre>
+        {prettyFormat(
+          this.state,
+          {
+            plugins: [reactElementPlugin]
+          }
+        )}
+      </pre>
+    );
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,46 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactNoop from 'react-dom/fiber-noop';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import App from './App';
 import Target from './Target'
-import reducer from './reducer';
 import './index.css';
 
-let root;
-const store = createStore(
-  reducer,
-  root,
-  window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const ReactDebugNoop = ReactNoop.create({
-  onMountContainer(r) {
-    root = r;
-  },
-
-  onBeginWork() {
-    store.dispatch({
-      type: 'BEGIN_WORK',
-      fiberRoot: root.current,
-    });
-  },
-
-  onCompleteWork() {
-    store.dispatch({
-      type: 'COMPLETE_WORK',
-      fiberRoot: root.current,
-    });
-  },
-});
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App render={renderer => {
+    renderer.render(<Target />);
+    renderer.flush();
+  }} />,
   document.getElementById('root')
 );
-
-ReactDebugNoop.render(<Target/>);
-ReactDebugNoop.flush();
